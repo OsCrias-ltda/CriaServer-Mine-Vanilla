f56d2d17764b9de4c1dc0b689189e603d0fe8e09
rayh <raykah.silva@gmail.com>
2023-07-18 19:47:23 +0000

    adicionando a ferramenta e docker compose

---
+++ mine.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+
+if [ $1 = 'setup' ]; then
+    echo 'iniciando o setup do servidor'
+    sudo docker run -d -v .:/cpy:rw --name server imrayh/server-vanila cp -r /server /cpy
+    sudo docker exec server cp -r /server /cpy
+    sudo docker stop server && sudo docker rm server
+    sudo chown -R ubuntu:ubuntu server/
+
+    # subindo para finalizar!
+    sudo docker compose up -d
+
+elif [ $1 = 'stop' ]
+ then
+    sudo docker compose down
+
+elif  [ $1 = 'restart' ]
+ then
+  sudo docker compose restart
+
+elif [ $1 = 'prune' ]; then
+    echo 'tem certeza que quer apagar tudo?'
+    read resp
+    if [ resp = 'y' ]; then
+        sudo docker compose down
+            sudo rm -r server/
+    fi
+elif [ $1 = 'start' ]; then
+    sudo docker compose up -d
+else
+  echo 'nenhum parâmetro oferecido!'
+fi
